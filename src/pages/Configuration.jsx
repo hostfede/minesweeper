f897ca8a90ef128b2cdf1147497579a31397a5b7
Federico Host <federico.host@gmail.com>
2024-07-23 11:25:41 -0300

    feat: creation of configuration page

---
+++ src/pages/Configuration.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header/Header';
+
+function Configuration() {
+  return (
+    <div>
+      <Header />
+      <h1>CONFIGURATION</h1>
+    </div>
+  );
+}
+
+export default Configuration;
