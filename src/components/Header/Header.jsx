75a921ca984c81fda22ef5a517a12541af68be45
Federico Host <federico.host@gmail.com>
2024-07-25 12:47:25 -0300

    refactor(header): change image --> title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { NavBar } from './Navbar/Nav';
 function Header() {
   return (
     <header>
-      <img src="./images/minesweeper.png" width={200} />
+      <div className="tittle">Minesweeper</div>
       <NavBar />
     </header>
   );
